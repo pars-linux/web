b072e047903c5b7841065ead27b7f102a48e4fca
jnmbk <jnmbk@255603d7-ef53-460c-a3be-e468e87e5197>
2009-05-16 14:41:45 +0000

    get news from database

--- trunk/web/chiq/st/views.py
+++ trunk/web/chiq/st/views.py
@@ -7,12 +7,15 @@
 
 from chiq.flatpages.models import FlatPage
 from chiq.st.wrappers import render_response
+from chiq.st.models import News
 
 def robots(request):
     return render_response(request, 'robots.txt')
 
 def home(request):
     pages = FlatPage.objects.all()
+    mainnews = News.objects.filter(is_main=True, is_published=True).order_by("date")[:1]
+    news = News.objects.filter(is_main=False, is_published=True).order_by("date")[:2]
     return render_response(request, 'home.html', locals())
 
 def tag_detail(request, tag):
