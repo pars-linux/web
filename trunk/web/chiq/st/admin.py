dea3d4e05ed19ffcad50d268c955c5433648cc3f
jnmbk <jnmbk@255603d7-ef53-460c-a3be-e468e87e5197>
2009-05-16 11:42:36 +0000

    initial news application

--- trunk/web/chiq/st/admin.py
+++ trunk/web/chiq/st/admin.py
@@ -7,7 +7,7 @@
 
 from django.contrib import admin
 
-from chiq.st.models import Tag, OtherFile
+from chiq.st.models import News, Tag, OtherFile
 
 class StSimpleAdmin(admin.ModelAdmin):
     list_display = ('name', 'id')
@@ -19,5 +19,24 @@ class FileAdmin(admin.ModelAdmin):
     ordering = ['-id']
     search_fields = ['file', 'desc']
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'date')
+    list_filter = ('date',)
+    ordering = ('date',)
+    search_fields = ('title', 'text', 'sum', 'tag__name')
+    prepopulated_fields = {'slug': ("title",)}
+
+    fieldsets = (
+            ('Genel', {'fields': ('title', 'image', 'sum', 'text', 'tags', 'date', "is_published")}),
+            ('Diğer', {'fields': ('slug',), 'classes': 'collapse'}),
+            )
+
+    class Media:
+        js = ("js/tinymce/tiny_mce.js", "js/tinymce/textareas.js", "js/jquery-1.2.6.min.js", "js/adminimages.js", "js/jquery.autocomplete.js", "js/taghelper.js")
+        css = {
+            "all": ("css/autocomplete.css",),
+        }
+
 admin.site.register(Tag, StSimpleAdmin)
 admin.site.register(OtherFile, FileAdmin)
+admin.site.register(News, NewsAdmin)
