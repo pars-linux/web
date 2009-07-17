d2c70287522a5f50fd3c00dab352738a344bfc11
erenturkay <erenturkay@255603d7-ef53-460c-a3be-e468e87e5197>
2009-07-17 23:54:12 +0000

    more more

---
+++ trunk/web/chiq/eng-templates/basin/views.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TÜBİTAK UEKAE
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+import datetime
+
+from django.shortcuts import get_object_or_404
+
+from chiq.basin.models import *
+from chiq.st.wrappers import render_response
+
+def bulletin_detail(request, slug):
+    bulletin = get_object_or_404(Bulletin, slug=slug)
+    return render_response(request, "basin/bulten.html", locals())
+
+def main(request):
+    bulletins = Bulletin.objects.filter(is_published=True).order_by("-date")
+    return render_response(request, "basin/bultenlervegorseller.html", locals())
+
+def year(request, year):
+    #TODO: Dynamically populate years from database
+    years = ("2009","2008","2007","2006","2005")
+    issues = Issue.objects.filter(date__year=year)
+    return render_response(request, "basin/liste.html", locals())
+
+def page(request, year, month, day, slug, page):
+    page = get_object_or_404(Page, issue__date=datetime.date(int(year),int(month),int(day)), issue__publication__slug=slug, number=int(page))
+    return render_response(request, "basin/sayfa.html", locals())
---
+++ trunk/web/chiq/eng-templates/indir/admin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TÜBİTAK UEKAE
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from django.contrib import admin
+
+from chiq.indir.models import Download, Version
+
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ("name", "status")
+    ordering = ["-name"]
+    search_fields = ["name", "release_notes"]
+
+    class Media:
+        js = ("js/tinymce/tiny_mce.js", "js/tinymce/textareas.js")
+
+class DownloadAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "status")
+    ordering = ["version"]
+    search_fields = ["title", "description"]
+
+admin.site.register(Version, VersionAdmin)
+admin.site.register(Download, DownloadAdmin)
