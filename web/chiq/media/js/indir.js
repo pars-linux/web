31b04d425ff4038569a5114e4eefa4191abd2328
Uğur Çetin <ugur@ugurcetin.com.tr>
2009-07-17 13:11:50 +0000

    no live CD for now

--- web/chiq/media/js/indir.js
+++ web/chiq/media/js/indir.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
         $(".calisan").addClass("selected");
         $(".calisanindir").show();
         $(".kurulanindir").hide();
+        $(".ustaciklama").text("Çalışan CD sürümümüz yakında yayınlanacaktır.");
     });
     $(".kurulan").click(function(){
         $(".calisan").removeClass("selected");
         $(".kurulan").addClass("selected");
         $(".calisanindir").hide();
         $(".kurulanindir").show();
+        $(".ustaciklama").text("Bir sürüm seçin.");
     });
 });
