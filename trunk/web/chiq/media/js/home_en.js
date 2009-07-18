a7f3915ec3b743c8c8f9c38cd559095c63f52b54
jnmbk <jnmbk@255603d7-ef53-460c-a3be-e468e87e5197>
2009-07-18 00:21:29 +0000

    more box

--- trunk/web/chiq/media/js/home_en.js
+++ trunk/web/chiq/media/js/home_en.js
@@ -1,12 +1,12 @@
 function kutusec(kutu){
-    if (kutu == 1) $("#kutu1").attr("src", "/media/resimler/kutu/metin_kurumsal02.png");
-    else $("#kutu1").attr("src", "/media/resimler/kutu/metin_kurumsal01.png");
-    if (kutu == 2) $("#kutu2").attr("src", "/media/resimler/kutu/metin_bireysel02.png");
-    else $("#kutu2").attr("src", "/media/resimler/kutu/metin_bireysel01.png");
-    if (kutu == 3) $("#kutu3").attr("src", "/media/resimler/kutu/metin_gelistirici02.png");
-    else $("#kutu3").attr("src", "/media/resimler/kutu/metin_gelistirici01.png");
-    if (kutu == 4) $("#kutu4").attr("src", "/media/resimler/kutu/metin_cozumortaklari02.png");
-    else $("#kutu4").attr("src", "/media/resimler/kutu/metin_cozumortaklari01.png");
+    if (kutu == 1) $("#kutu1").attr("src", "/media/resimler/kutu/corporate_02.png");
+    else $("#kutu1").attr("src", "/media/resimler/kutu/corporate_01.png");
+    if (kutu == 2) $("#kutu2").attr("src", "/media/resimler/kutu/enduser_02.png");
+    else $("#kutu2").attr("src", "/media/resimler/kutu/enduser_01.png");
+    if (kutu == 3) $("#kutu3").attr("src", "/media/resimler/kutu/developer_02.png");
+    else $("#kutu3").attr("src", "/media/resimler/kutu/developer_01.png");
+    if (kutu == 4) $("#kutu4").attr("src", "/media/resimler/kutu/partner_02.png");
+    else $("#kutu4").attr("src", "/media/resimler/kutu/partner_01.png");
 }
 $(document).ready(function() {
     $("#kutu1").mouseover(function(){
