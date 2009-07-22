f004d429c293a6cdddee89043f2fbc5a54a75ff8
Uğur Çetin <ugur@ugurcetin.com.tr>
2009-07-22 10:39:09 +0000

    lines and arrows

--- web/chiq/media/js/iletisim.js
+++ web/chiq/media/js/iletisim.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         $(".kurumsal_secici").removeClass("iletisim_secili");
         $(".soru_sorun_secici").addClass("iletisim_secili");
         $(".sirket_alani").hide();
+        $("#ok").css("margin-left", "170px");
         $(".iletisim_aciklama_baslik").text("Bireysel sorularınız için");
         $(".iletisim_aciklama_metin").text("Bireysel sorularınızı buradan sorabilirsiniz");
     });
@@ -10,6 +11,7 @@ $(document).ready(function() {
         $(".soru_sorun_secici").removeClass("iletisim_secili");
         $(".kurumsal_secici").addClass("iletisim_secili");
         $(".sirket_alani").show();
+        $("#ok").css("margin-left", "60px");
         $(".iletisim_aciklama_baslik").text("Kurumsal sorularınız için");
         $(".iletisim_aciklama_metin").text("Kurumsal sorularınızı buradan sorabilirsiniz");
     });
